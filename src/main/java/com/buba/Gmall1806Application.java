15f909488b55da4a8a025a4121cfb9a664506096
jia <jia417779025@qq.com>
2020-04-13 15:06:08 +0800

    创建项目 --jia

---
+++ src/main/java/com/buba/Gmall1806Application.java
@@ -0,0 +1,13 @@
+package com.buba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall1806Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall1806Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/buba/Gmall1806ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall1806ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
